82dc12abcd5e92b72d9c4d1bbbd698dc407ef62f
VORNI4ESKU <iravorni@gmail.com>
2018-04-09 23:49:54 +0300

    Home work 5

---
+++ Home work 5/main.js
@@ -0,0 +1,71 @@
+; (function () {
+    var selectValue;
+    var dateFrom;
+    var dateTo;
+    var adventureSelector = document.getElementById("adventure");
+    for (let index = 0; index < data.genres.length; index++) {
+        var option = document.createElement("option");
+        option.value = data.genres[index];
+        option.innerText = data.genres[index];
+        adventureSelector.appendChild(option);
+    }
+    filter();
+
+    document.getElementById("find").onclick = filter;
+    
+    function filter() {
+        var movies = data.movies;
+
+        var e = document.getElementById("adventure");
+        selectValue = e.options[e.selectedIndex].value;
+        dateFrom = document.getElementById("from").value;
+        dateTo = document.getElementById("to").value;
+
+
+        movies = movies.filter(filterByGenres);
+        movies = movies.filter(filterByDate);
+
+        var filmTable = document.querySelector("#filmTable tbody")
+        filmTable.innerHTML = "";
+        for (let index = 0; index < movies.length; index++) {
+            var tr = document.createElement("tr");
+
+            var td = document.createElement("td");
+            td.innerText = movies[index].title;
+            tr.appendChild(td);
+
+            td = document.createElement("td");
+            td.innerText = movies[index].genres.join(", ");
+            tr.appendChild(td);
+
+            td = document.createElement("td");
+            td.innerText = movies[index].year;
+            tr.appendChild(td);
+
+            filmTable.appendChild(tr);
+        }
+
+        function filterByGenres(item) { 
+            if (!selectValue)
+                return true;
+                console.log(item.genres)
+                console.log(item.genres.indexOf(selectValue))
+            
+            return item.genres.indexOf(selectValue)>0;
+        }
+        function filterByDate(item) {
+            var filterValue = true;
+            console.log()
+            if(dateFrom) {
+                filterValue = item.year >= dateFrom
+            }
+            if(filterValue && dateTo) {
+                filterValue = item.year <= dateTo
+            }
+            return filterValue;
+        }
+    }
+})()
+
+
+
